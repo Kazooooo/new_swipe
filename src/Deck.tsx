fb7943137c30a22ea1b3d5007f63b9b69afa7bcc
Kazooooo <cest93@gmail.com>
2018-03-29 00:32:14 +0900

    PanResponderMoveでdx dyのpositionでDeckをAnimeteさせる

--- src/Deck.tsx
+++ src/Deck.tsx
@@ -9,29 +9,32 @@ interface DeckProps {
 
 interface DeckState {
   panResponder: PanResponderInstance;
+  position: Animated.ValueXY;
 }
 
 class Deck extends React.Component<DeckProps, DeckState> {
   constructor(props: DeckProps) {
     super(props);
+
+    const position = new Animated.ValueXY();
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-        console.log(gesture);
+        position.setValue({ x: gesture.dx, y: gesture.dy });
       },
       onPanResponderRelease: () => {},
     });
-    this.state = { panResponder };
+    this.state = { panResponder, position };
   }
 
   render() {
     const { cardDataList } = this.props;
-    const { panResponder } = this.state;
+    const { panResponder, position } = this.state;
 
     return (
-      <View {...panResponder.panHandlers}>
+      <Animated.View style={position.getLayout()} {...panResponder.panHandlers}>
         {cardDataList.map((cardData) => <DeckCard key={cardData.id} cardData={cardData} />)}
-      </View>
+      </Animated.View>
     );
   }
 }
