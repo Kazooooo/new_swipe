50eff32aab957f68eb092bb3521852aa459fd6fb
Kazooooo <cest93@gmail.com>
2018-03-28 23:52:41 +0900

    DeckCardのUIをカスタマイズ

--- src/DeckCard.tsx
+++ src/DeckCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text } from "react-native";
+import { Card, Button } from "react-native-elements";
 import { CardData } from "./App";
 
 interface DeckCardProps {
@@ -9,7 +10,17 @@ interface DeckCardProps {
 class DeckCard extends React.Component<DeckCardProps, {}> {
   render() {
     const { cardData } = this.props;
-    return <Text>{cardData.text}</Text>;
+    return (
+      <Card title={cardData.text} image={{ uri: cardData.uri }}>
+        <Text style={{ marginBottom: 10 }}>I can customize the Card further.</Text>
+        <Button
+          icon={{ name: "code" }}
+          backgroundColor="#03A9F4"
+          title="View Now!"
+          onPress={() => {}}
+        />
+      </Card>
+    );
   }
 }
 
