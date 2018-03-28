79443fb124fe855508057ced8d845f06555ebc26
Kazooooo <cest93@gmail.com>
2018-03-29 01:06:08 +0900

    AnimatedDeckCardのXに応じてrotateさせる

--- src/AnimatedDeckCard.tsx
+++ src/AnimatedDeckCard.tsx
@@ -9,8 +9,21 @@ interface AnimatedDeckCardProps extends DeckCardProps {
 
 const AnimatedDeckCard: React.SFC<AnimatedDeckCardProps> = (props) => {
   const { panResponder, position, cardData } = props;
+
+  const getAnimatedDeckCardStyle = () => {
+    const rotate = position.x.interpolate({
+      inputRange: [-500, 0, 500],
+      outputRange: ["-120deg", "0deg", "120deg"],
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }],
+    };
+  };
+
   return (
-    <Animated.View style={position.getLayout()} {...panResponder.panHandlers}>
+    <Animated.View style={getAnimatedDeckCardStyle()} {...panResponder.panHandlers}>
       <DeckCard cardData={cardData} />
     </Animated.View>
   );
