cc855a823b4b2bc8e01d3f5d140f1e2da9879db4
Kazooooo <cest93@gmail.com>
2018-03-29 00:19:01 +0900

    DeckにPanResponderを設定

--- src/Deck.tsx
+++ src/Deck.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Animated } from "react-native";
+import { View, Animated, PanResponder, PanResponderInstance } from "react-native";
 import { CardData } from "./App";
 import DeckCard from "./DeckCard";
 
@@ -7,11 +7,29 @@ interface DeckProps {
   cardDataList: CardData[];
 }
 
-class Deck extends React.Component<DeckProps, {}> {
+interface DeckState {
+  panResponder: PanResponderInstance;
+}
+
+class Deck extends React.Component<DeckProps, DeckState> {
+  constructor(props: DeckProps) {
+    super(props);
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        console.log(gesture);
+      },
+      onPanResponderRelease: () => {},
+    });
+    this.state = { panResponder };
+  }
+
   render() {
     const { cardDataList } = this.props;
+    const { panResponder } = this.state;
+
     return (
-      <View>
+      <View {...panResponder.panHandlers}>
         {cardDataList.map((cardData) => <DeckCard key={cardData.id} cardData={cardData} />)}
       </View>
     );
